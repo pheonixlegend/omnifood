8277641ef41807f24b36695cadce417c55fe06ad
pheonixlegend <jpnc2014@gmail.com>
2018-09-12 21:22:54 +0200

    Finished setting up sticky navigation

---
+++ resources/js/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+
+    $('.js--section-features').waypoint(function(direction) {
+        if (direction == "down") {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        }
+    }, {
+        offset: '60px;'
+    });
+
+    /*
+
+    var waypoint = new Waypoint({
+      element: document.getElementById('px-offset-waypoint'),
+      handler: function(direction) {
+        notify('I am 20px from the top of the window')
+      },
+      offset: 20
+})
+    */
+
+});
